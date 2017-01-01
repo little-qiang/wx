172a9f4b9814e70ece79df63d5403acd2c5cb4bc
little-qiang <346669517@qq.com>
2017-01-01 12:01:36 +0800

    x

--- app/Http/Controllers/Wx/MenuController.php
+++ app/Http/Controllers/Wx/MenuController.php
@@ -10,36 +10,70 @@ class MenuController extends Controller
 {
 	public function create()
 	{
-		$accesstoken = 'fzPLv--CjEIG5drsypbTXEwnAgtIF_LFDlXXNOsSC3rMcxNg5AaYYtXhA1pgbDpYH2ZpDAoU8OiJ3yKiYmhSaS6k_3ffzv_KUAhLASsnIbnIEwkPG_w7pBlwkJNooowxEBOhABAAXK';
+		$accesstoken = 'clgf2cqKmmLKvrK5lYwYFJW-cZiD7Zyils4Edt7hotZ8rqV7rNb6JIdCXok1lowz6WuyJ0EWRDt9j9739tWXLBv236O1MyVvv6tq9BNGNY38J5zeNtvMz0Ul5xyDw3t_FTNcAGAIWQ';
 		$url = sprintf("https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s", $accesstoken);
-    	$paramsStr = '{
-     		"button":[
-	    	{	
-				"type":"click",
-				"name":"今日歌曲",
-				"key":"V1001_TODAY_MUSIC"
-		    },
-			{
-				"name":"菜单",
-				"sub_button":[
-				{	
-					"type":"view",
-					"name":"搜索",
-					"url":"http://www.soso.com/"
-				},
-				{
-					"type":"view",
-					"name":"视频",
-					"url":"http://v.qq.com/"
-				},
-				{
-					"type":"click",
-					"name":"赞一下我们",
-					"key":"V1001_GOOD"
-				}]
-	    	}]
-		}';
-		$bool = Curl::to($url)->withData(json_decode($paramsStr, true))->post();
+    	$params = [
+    		"button" => [
+    			[
+    				"type" => "click",
+		          	"name" => "今日歌曲",
+		          	"key" => "V1001_TODAY_MUSIC"
+    			],
+    			[
+    				"name" => "菜单",
+		           	"sub_button" => [
+		           		[
+           					"type" => "view",
+		               		"name" => "搜索",
+		               		"url" => "http://www.soso.com/"
+		           		],
+		           		[
+		           			"type" => "view",
+		               		"name" => "视频",
+		               		"url" => "http://v.qq.com/"
+		           		],
+		           		[
+		           			"type" => "click",
+		               		"name" => "赞一下我们",
+		               		"key" => "V1001_GOOD"
+		           		]
+	           		]
+    			]
+    		]
+    	];
+
+    	$param1 = '{
+     "button":[
+     {	
+          "type":"click",
+          "name":"今日歌曲",
+          "key":"V1001_TODAY_MUSIC"
+      },
+      {
+           "name":"菜单",
+           "sub_button":[
+           {	
+               "type":"view",
+               "name":"搜索11",
+               "url":"http://www.soso.com/"
+            },
+            {
+               "type":"view",
+               "name":"视频",
+               "url":"http://v.qq.com/"
+            },
+            {
+               "type":"click",
+               "name":"赞一下我们",
+               "key":"V1001_GOOD"
+            }]
+       }]
+ }';
+		$bool = Curl::to($url)
+				->withHeader( 'Content-Type: application/json' )
+				->withData($param1)
+				// ->withData(json_encode($params, JSON_UNESCAPED_UNICODE))
+				->post();
 		dd($bool);
 	}
 }
